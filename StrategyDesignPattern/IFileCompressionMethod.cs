abf4588551989b822b3e929245250b814e2d19b8
NEERAJ KRISHNA N <neerajnjps03@gmail.com>
2024-08-27 08:26:33 +0530

    Add interface for File compression and implemented a strategy TarCompression

---
+++ StrategyDesignPattern/IFileCompressionMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace StrategyDesignPattern
+{
+    public interface IFileCompressionMethod
+    {
+        public void Compress(string filename);
+
+        public void DeCompress(string filename);
+    }
+}
